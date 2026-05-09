a8d6d9b8c3be61e82acd0b4115bc0c08c675939e
Anas Mahmoud <108479517+Anas-M96@users.noreply.github.com>
2026-05-09 19:17:30 +0300

    Create The PADS.sql

---
+++ Advanced Select/The PADS.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(name, '(', LEFT(occupation,1), ')')
+FROM OCCUPATIONS
+ORDER BY name;
+
+
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY occupation
+ORDER BY COUNT(*) ASC, occupation ASC;
